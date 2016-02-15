69c4e7231e313af70120c4b5efccb6668afa2ab2
Akiroz <akiroz@users.noreply.github.com>
2016-02-15 17:05:25 +0800

    Create sshbookmark.pl

---
+++ sshbookmark.pl
@@ -0,0 +1,28 @@
+#!/bin/perl
+
+my $in = 0;
+if(@ARGV == 1) {
+    $in = $1;
+} else {
+    print
+    "1: login to foo\n" .
+    "2: login to bar\n" .
+    "h: <user>\@<fqdn>[:<port>]\n" .
+    "Enter host or bookmark: ";
+    chomp($in = <STDIN>);
+}
+
+if($in =~ /^[0-9]$/) {
+    if($in == 1) {exec 'ssh user@foo.com -p 22001';}
+    if($in == 2) {exec 'ssh user@bar.com -p 22002';}
+    print "Invalid bookmark number.\n"; die;
+}
+
+print "[<local>:]<port>:<remote>:<port>\n".
+"Enter tunnel: ";
+chomp(my $tunnel = <STDIN>);
+
+my ($host,$port) = split /:/, $in;
+if($port !~ /[0-9]/) {$port = 22};
+
+exec "ssh -f $host -p $port -L $tunnel -N";
